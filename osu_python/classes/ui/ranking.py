1eac30588bf37c6a646413d3bf0c55e6b332a453
suvoshi <sskk0v@gmail.com>
2023-02-24 14:36:23 +0900

    ranking btns added

---
+++ osu_python/classes/ui/ranking.py
@@ -0,0 +1,73 @@
+import pygame as pg
+from osu_python.classes.ui import root
+from osu_python.classes import Library, Config
+
+
+def load_skin():
+    global ButtonRetry, ButtonReplay
+
+    path = Config.base_path + "/skins/" + Config.cfg["skin"]
+
+    ButtonRetry.im = pg.image.load(path + "/pause-retry.png")
+    ButtonReplay.im = pg.image.load(path + "/pause-replay.png")
+
+
+class Button(root.UiElement):
+    def __init__(self, height: int, width: int):
+        super().__init__(True, True)
+
+        self.width, self.height = width, height
+
+        self.y_p = self.height / 10
+        self.x_p = self.width / 17.5
+
+        self.clicked = False
+    
+    def draw(self, screen: pg.Surface, dt: float):
+        if self.hover:
+            screen.blit(self.im.set_alpha(255), self.rect)
+        else:
+            screen.blit(self.im, self.rect)
+    
+    def click(self):
+        self.clicked = True
+    
+    def is_colliding(self, coords: tuple):
+        if self.rect.collidepoint(coords):
+            return True
+
+
+class ButtonRetry(Button):
+    im = None
+
+    def __init__(self, height: int, width: int):
+        super().__init__(height, width)
+
+        self.im = pg.transform.scale(
+            ButtonRetry.im, (round(self.x_p * 2), round(self.y_p))
+        ).convert_alpha()
+
+        self.im.set_alpha(160)
+
+        self.x, self.y = round(self.x_p * 14), round(self.y_p * 7)
+
+        self.rect = self.im.get_rect()
+        self.rect.left, self.rect.top = self.x, self.y
+
+
+class ButtonReplay(Button):
+    im = None
+
+    def __init__(self, height: int, width: int):
+        super().__init__(height, width)
+
+        self.im = pg.transform.scale(
+            ButtonReplay.im, (round(self.x_p * 2), round(self.y_p))
+        ).convert_alpha()
+
+        self.im.set_alpha(160)
+
+        self.x, self.y = round(self.x_p * 14), round(self.y_p * 8.5)
+
+        self.rect = self.im.get_rect()
+        self.rect.left, self.rect.top = self.x, self.y
--- osu_python/scenes/ranking.py
+++ osu_python/scenes/ranking.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from osu_python.classes import Config
 from osu_python.classes import ui
+import random
 
 
 all_objects = []
@@ -23,12 +24,13 @@ def update(events):
 
 
 def draw(screen):
-    for obj in all_objects:
-        obj.draw()
+    draw_background(screen)
+    draw_title(screen)
+    draw_ranking_panel(screen)
 
 
-def setup(_height, _width, _screen, _diff_path, _retry_func, _rank):
-    global height, width, screen, diff_path, retry_func, rank
+def setup(_height, _width, _screen, _diff_path, _retry_func, _rank, _draw_bg_func, _score, _results, _combo, _accuracy):
+    global height, width, screen, diff_path, retry_func, rank, draw_bg_func, score, results, combo, accuracy, btn_retry, btn_replay, mgr_btns, ranking_panel
 
     height = _height
     width = _width
@@ -36,8 +38,32 @@ def setup(_height, _width, _screen, _diff_path, _retry_func, _rank):
     diff_path = _diff_path
     retry_func = _retry_func
     rank = _rank
+    draw_bg_func = _draw_bg_func
+    score = _score
+    results = _results
+    combo = _combo
+    accuracy = _accuracy
+
+    btn_retry = ui.ranking.ButtonRetry(height, width)
+    btn_replay = ui.ranking.ButtonReplay(height, width)
+
+    mgr_btns = ui.root.UiManager([btn_retry, btn_replay])
+
+    ranking_panel = pg.imahe.load(Config.base_path + "/skins/" + Config.cfg["skin"] + "/ranking_panel.png")
 
 
 def tick(dt, events):
     update(events)
     draw(screen)
+
+
+def draw_background(screen):
+    draw_bg_func()
+
+
+def draw_title(screen):
+    pg.draw.rect(screen, (0, 0, 0) (0, 0, width, round(height * (2 / 13))))
+
+
+def draw_ranking_panel(screen):
+    screen.blit(ranking_panel, (0, round(height * (2 / 13)) + 20))
