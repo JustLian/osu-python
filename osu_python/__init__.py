3de0bc6a4a78fcca15a8cfc5bc0a6198521802a8
JustLian <why.lians.mail@gmail.com>
2023-02-26 20:36:07 +0900

    bumped version

--- osu_python/__init__.py
+++ osu_python/__init__.py
@@ -1,4 +1,4 @@
 __productname__ = "osu!python"
-__version__ = "1.0.0a"
+__version__ = "1.1"
 __description__ = "osu!std beatmaps opener written using Python and PyGame"
 __url__ = "https://github.com/JustLian/osu-python"
